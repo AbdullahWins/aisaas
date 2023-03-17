c41e4fa944346a09d8d33ad18e64dd3c60fa25a1
AbdullahWins <noyononline@gmail.com>
2023-03-17 20:04:05 +0600

    added auto emain capture

--- src/stripe/Checkout.js
+++ src/stripe/Checkout.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Checkout = () => {
-  const [email, setEmail] = useState("");
+  const { user } = useContext(AuthContext);
   const [plan, setPlan] = useState("price_1MkQZnFSVT0dmAsfrQ9iVzxO");
 
   const handleSubscribe = async () => {
-    const stripe = window.Stripe(process.env.REACT_APP_stripeKey);
+    const stripe = window.Stripe(process.env.REACT_APP_stripePublicKey);
     const { error } = await stripe.redirectToCheckout({
       mode: "subscription",
       lineItems: [{ price: plan, quantity: 1 }],
-      customerEmail: email,
-      successUrl: "https://your_website.com/success",
-      cancelUrl: "https://your_website.com/cancel",
+      customerEmail: user?.email,
+      successUrl: "https://aisaastools.netlify.app/",
+      cancelUrl: "https://aisaastools.netlify.app/",
     });
 
     if (error) {
@@ -20,14 +21,8 @@ const Checkout = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center bg-whiteHigh p-12 rounded-xl">
-      <h1>Subscribe to our service</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="flex flex-col items-center justify-center gap-4 bg-whiteHigh p-12 rounded-xl">
+      <h1>Subscribe to our service with {user?.email}</h1>
       <select value={plan} onChange={(e) => setPlan(e.target.value)}>
         <option value="price_1MkQZnFSVT0dmAsfrQ9iVzxO">
           Regular Subscription ($29/month)
